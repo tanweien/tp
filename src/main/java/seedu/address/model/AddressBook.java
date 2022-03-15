4710f9dd91d039fc0be4bf4f9f6502e45e87686e
eugenechiaay <eugene.chiay@gmail.com>
2022-03-16 01:14:18 +0800

    checkstyle

--- src/main/java/seedu/address/model/AddressBook.java
+++ src/main/java/seedu/address/model/AddressBook.java
@@ -23,7 +23,6 @@ public class AddressBook implements ReadOnlyAddressBook {
      * Note that non-static init blocks are not recommended to use. There are other ways to avoid duplication
      *   among constructors.
      */
-    
     public AddressBook() {
         persons = new UniquePersonList();
     }
