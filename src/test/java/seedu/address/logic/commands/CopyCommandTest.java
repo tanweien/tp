ff49a8e4447ef7088ee51fe106009457e6dd4a58
Sim Jia Ming <75411350+SimJM@users.noreply.github.com>
2022-03-25 02:14:30 +0800

    Update CopyCommandTest.java

--- src/test/java/seedu/address/logic/commands/CopyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CopyCommandTest.java
@@ -2,44 +2,12 @@ package seedu.address.logic.commands;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
-import static seedu.address.logic.commands.CommandTestUtil.showPersonAtIndex;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
-import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
 import org.junit.jupiter.api.Test;
 
-import seedu.address.model.Model;
-import seedu.address.model.ModelManager;
-import seedu.address.model.UserPrefs;
-import seedu.address.model.person.Person;
-
 public class CopyCommandTest {
-
-    private final Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-
-    @Test
-    public void execute_validIndexUnfilteredList_success() {
-        Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
-
-        String expectedMessage = String.format(CopyCommand.MESSAGE_COPY_PERSON_SUCCESS, personToCopy.getEmail());
-
-        assertCommandSuccess(copyCommand, model, expectedMessage, model);
-    }
-
-    @Test
-    public void execute_validIndexFilteredList_success() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
-
-        Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
-
-        String expectedMessage = String.format(CopyCommand.MESSAGE_COPY_PERSON_SUCCESS, personToCopy.getEmail());
-
-        assertCommandSuccess(copyCommand, model, expectedMessage, model);
-    }
 
     @Test
     public void equals() {
