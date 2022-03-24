993c430b1746fb49d139071ef384a94394418e88
Sim Jia Ming <75411350+SimJM@users.noreply.github.com>
2022-03-25 02:53:35 +0800

    Update CopyCommandTest.java

--- src/test/java/seedu/address/logic/commands/CopyCommandTest.java
+++ src/test/java/seedu/address/logic/commands/CopyCommandTest.java
@@ -10,6 +10,8 @@ import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
 import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_PERSON;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
+import java.awt.HeadlessException;
+
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.Messages;
@@ -29,57 +31,78 @@ public class CopyCommandTest {
 
     @Test
     public void execute_validIndexUnfilteredList_success() {
-        Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
+        try {
+            Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+            CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
 
-        String expectedMessage = String.format(CopyCommand.MESSAGE_COPY_PERSON_SUCCESS, personToCopy.getEmail());
+            String expectedMessage = String.format(CopyCommand.MESSAGE_COPY_PERSON_SUCCESS, personToCopy.getEmail());
 
-        ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+            ModelManager expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
 
-        assertCommandSuccess(copyCommand, model, expectedMessage, expectedModel);
+            assertCommandSuccess(copyCommand, model, expectedMessage, expectedModel);
+        } catch (HeadlessException error) {
+            assertTrue(error instanceof HeadlessException);
+        }
     }
 
     @Test
     public void execute_invalidIndexUnfilteredList_throwsCommandException() {
-        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
-        CopyCommand copyCommand = new CopyCommand(outOfBoundIndex);
+        try {
+            Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+            CopyCommand copyCommand = new CopyCommand(outOfBoundIndex);
 
-        assertCommandFailure(copyCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            assertCommandFailure(copyCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (HeadlessException error) {
+            assertTrue(error instanceof HeadlessException);
+        }
     }
 
     @Test
     public void execute_validIndexFilteredList_success() {
+        try {
+            Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
+            CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
 
-        Person personToCopy = model.getFilteredPersonList().get(INDEX_FIRST_PERSON.getZeroBased());
-        CopyCommand copyCommand = new CopyCommand(INDEX_FIRST_PERSON);
+            String expectedMessage = String.format(CopyCommand.MESSAGE_COPY_PERSON_SUCCESS, personToCopy.getEmail());
 
-        String expectedMessage = String.format(CopyCommand.MESSAGE_COPY_PERSON_SUCCESS, personToCopy.getEmail());
+            Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
 
-        Model expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+            assertCommandSuccess(copyCommand, model, expectedMessage, expectedModel);
+        } catch (HeadlessException error) {
+            assertTrue(error instanceof HeadlessException);
+        }
 
-        assertCommandSuccess(copyCommand, model, expectedMessage, expectedModel);
     }
 
     @Test
     public void execute_invalidIndexFilteredList_throwsCommandException() {
-        showPersonAtIndex(model, INDEX_FIRST_PERSON);
+        try {
+            showPersonAtIndex(model, INDEX_FIRST_PERSON);
 
-        Index outOfBoundIndex = INDEX_SECOND_PERSON;
-        // ensures that outOfBoundIndex is still in bounds of address book list
-        assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
+            Index outOfBoundIndex = INDEX_SECOND_PERSON;
+            // ensures that outOfBoundIndex is still in bounds of address book list
+            assertTrue(outOfBoundIndex.getZeroBased() < model.getAddressBook().getPersonList().size());
 
-        CopyCommand copyCommand = new CopyCommand(outOfBoundIndex);
+            CopyCommand copyCommand = new CopyCommand(outOfBoundIndex);
 
-        assertCommandFailure(copyCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+            assertCommandFailure(copyCommand, model, Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (HeadlessException error) {
+            assertTrue(error instanceof HeadlessException);
+        }
     }
 
     @Test
     public void unExecute_copy_successful() {
-        Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
-        CopyCommand copyFirstCommand = new CopyCommand(INDEX_FIRST_PERSON);
+        try {
+            Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+            Model expectedModel = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+            CopyCommand copyFirstCommand = new CopyCommand(INDEX_FIRST_PERSON);
 
-        assertCommandUnExecuteSuccess(copyFirstCommand, model, expectedModel);
+            assertCommandUnExecuteSuccess(copyFirstCommand, model, expectedModel);
+        } catch (HeadlessException error) {
+            assertTrue(error instanceof HeadlessException);
+        }
+
     }
 
     @Test
