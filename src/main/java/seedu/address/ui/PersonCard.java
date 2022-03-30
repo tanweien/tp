df2edcf9339e4ddb41d883b7bb38c1575589ab2e
Sim Jia Ming <75411350+SimJM@users.noreply.github.com>
2022-03-31 00:17:20 +0800

    Update PersonCard.java

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -19,10 +19,10 @@ public class PersonCard extends UiPart<Region> {
     private static final String heart = new String(Character.toChars(10084));
     private static final String empty = "";
     private static final String telegramField = "Telegram : ";
-    private static final String phoneField = "Phone   : ";
-    private static final String emailField = "Email   : ";
-    private static final String facultyField = "Faculty : ";
-    private static final String roleField = "Role    : ";
+    private static final String phoneField = "Phone    : ";
+    private static final String emailField = "Email    : ";
+    private static final String facultyField = "Faculty  : ";
+    private static final String roleField = "Role     : ";
 
 
     /**
